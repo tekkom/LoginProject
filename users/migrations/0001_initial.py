be18108f0c56cfedff08016efee5e8ddc92bfdac
Ogroger <30476473+Ogroger@users.noreply.github.com>
2017-07-27 00:04:01 +0200

    User tabels
    Update classes in models.

---
+++ users/migrations/0001_initial.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Group',
+            fields=[
+                ('id', models.AutoField(primary_key=True, serialize=False, auto_created=True, verbose_name='ID')),
+                ('gName', models.CharField(max_length=100)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='User',
+            fields=[
+                ('id', models.AutoField(primary_key=True, serialize=False, auto_created=True, verbose_name='ID')),
+                ('email', models.CharField(max_length=50)),
+                ('uName', models.CharField(max_length=100)),
+                ('verified', models.BooleanField(default=False)),
+                ('group', models.ForeignKey(to='users.Group')),
+            ],
+        ),
+    ]
