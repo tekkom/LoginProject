ac5b672a9de48b6638db2961af2251df087e91d4
Ogroger <30476473+Ogroger@users.noreply.github.com>
2017-07-27 17:44:59 +0200

    Test

--- users/views.py
+++ users/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponse
 # Create your views here.
 
-def index(request):
-    return render_to_response('users/index.html', {
-        'events' : User.objects.all()[:5]
-    })
+#def index(request):
+   # return render_to_response('users/index.html', {
+  #      'events' : User.objects.all()[:5]
+   # })
